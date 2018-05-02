3517331bccbdc3bb03fe1249d40a6113c02a3c39
imranmuhammad1518 <madhusparkyy@gmail.com>
2018-05-02 12:01:15 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  getUrl:string;
+  oppurtunityResponseData;
+  listResponseData;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.getUrl = 'http://gisapi-web-staging-1636833739.eu-west-1.elb.amazonaws.com/v2/opportunities/530?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c';
+    this.http.get(this.getUrl).subscribe(oppurtunityResponseData => {
+      this.oppurtunityResponseData  = oppurtunityResponseData;
+    })
+
+    this.http.get('http://gisapi-web-staging-1636833739.eu-west-1.elb.amazonaws.com/v2/opportunities/530?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c')
+    .subscribe(listResponseData =>{
+      this.listResponseData  = listResponseData;
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,72 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+import { MatButtonModule,
+  MatCardModule,
+  MatMenuModule, 
+  MatInputModule,
+  MatFormFieldModule,
+  MatTabsModule,
+  MatButtonToggleModule,
+  MatTableModule,
+  MatGridListModule,
+  MatSelectModule,
+  MatOptionModule,
+  MatCheckboxModule,
+  MatTooltipModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatRadioModule,
+  MatDividerModule,
+  MatListModule
+  } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
+import { OverviewComponent } from './overview/overview.component';
+import { GlobalEntrepreneurComponent } from './global-entrepreneur/global-entrepreneur.component';
+import { PrerequisitesComponent } from './prerequisites/prerequisites.component';
+import { VisaAndLogisticsComponent } from './visa-and-logistics/visa-and-logistics.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent,
+    GlobalEntrepreneurComponent,
+    PrerequisitesComponent,
+    VisaAndLogisticsComponent,
+    TestimonialsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatButtonToggleModule,
+    MatTableModule,
+    MatGridListModule,
+    FlexLayoutModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatCheckboxModule,
+    MatTooltipModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatRadioModule,
+    MatDividerModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
